69c1e42a25b1acbd73fd3988687bf5ce21501935
Anton Motornenko <motornenko@fias.uni-frankfurt.de>
2020-06-07 21:50:09 +0200

    added docstrings

--- tovsolver/tov.py
+++ tovsolver/tov.py
@@ -1,3 +1,24 @@
+## Copyright 2019-2020 Anton Motornenko (FIAS)
+
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""TOV solver class and methods
+"""
+
+__author__ = "Anton Motornenko (FIAS)"
+__copyright__ = "Copyright 2019-2020 Anton Motornenko (FIAS), GNU GPL 3"
+
 from scipy.interpolate import interp1d
 from scipy.integrate import odeint
 from scipy.optimize import root
@@ -9,7 +30,33 @@ import numpy as np
 from .constants import *
 
 class TOV:
-  def __init__(self, en_arr, p_arr, N=1000, add_crust=True, plot_eos=False):
+  def __init__(self, en_arr, p_arr, add_crust=True, plot_eos=False):
+    """
+    Initializes TOVsolver, EOS should be provided by passing pressure (p_arr)
+    as function of energy density (en_arr). p_arr and en_arr are given in nuclear
+    units (MeV/fm^3). By default adds nuclear crust from:
+    G. Baym, C. Pethick and P. Sutherland,
+    ``The Ground state of matter at high densities: Equation of state and stellar models,''
+    Astrophys. J. \textbf{170}, 299-317 (1971), doi:10.1086/151216.
+
+    Parameters
+    ----------
+    en_arr : array_like, MeV / fm^3
+             Array with range of energy densities of provides EOS, 
+             should be provided in MeV/fm^3.
+    p_arr  : array_like, MeV / fm^3
+             Array with pressure as fucntion of en_arr,
+             should be provided in MeV/fm^3.
+    add_crust : bool, optional
+             Merge EOS with crust EOS from nuclear statistical equilibrium.
+    plot_eos  : bool, optional
+             Output a plot with the EOS, optional.
+
+    Returns
+    -------
+    solver : TOV
+             Instance of tovsolver.
+    """
 
     en_arr *= MeV_fm3_to_pa_cgs / c**2
     p_arr  *= MeV_fm3_to_pa_cgs
@@ -50,6 +97,20 @@ class TOV:
       plt.show()
 
   def add_crust(self):
+    """
+    Adds Nuclear Statistical Equilibrium crust EOS from:
+    G. Baym, C. Pethick and P. Sutherland,
+    ``The Ground state of matter at high densities: Equation of state and stellar models,''
+    Astrophys. J. \textbf{170}, 299-317 (1971), doi:10.1086/151216.
+    Finds an intersection point between provided EOS and NSE EOS, then the two EOS are merged,
+    so at lower densities crust EOS is used and at higher -- the provided one.
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    """
     crust_loc = pkg_resources.resource_filename(__name__, 'data/')
     # dir_name = os.path.dirname(__file__)
     
@@ -153,8 +214,42 @@ class TOV:
 
 
   def solve(self, c_dens, rmax=30e5, rtol=1.0e-5, dmrel=10e-12, dr=100):
+    """
+    Solves TOV equation for neutron star with given central density c_dens.
+
+    Parameters
+    ----------
+    c_dens : float, MeV / fm^3
+    rmax : float, cm, optional
+      Maximal distance from the star center along which star profile is calculated.
+      If rmax is smaller then actual radius of the star, then Mass and Radius 
+      will be calculated wrong.
+    rtol : float, optional
+      Relative accuracy of ODE solver.
+    dmrel : float, optional
+      Relative mass increase by which star boundary is estimated. 
+    dr : float, cm, optinal
+      Stepsize for ODE solver.
+
+    Returns
+    -------
+    R : float, km
+      Calculated radius of the star.
+    M : float, Msun
+      Calculated mass of the star.
+    tuple (r, e_R, p_R, m_R) :
+      Neutron star profile.
+      r : numpy.array, cm
+        Array of points along which profile is calculated.
+      e_R : numpy.array
+        Energy density of the star along r.
+      p_R : numpy.array
+        Pressure of the star along r.
+      m_R : numpy.array
+        Integrated mass of the star along r.
+    """
     c_dens *= MeV_fm3_to_pa_cgs / c ** 2
-    N = self.N
+
     r = np.arange(dr, rmax + dr, dr)
 
     if c_dens < self.min_dens or c_dens > self.max_dens:
@@ -189,8 +284,41 @@ class TOV:
     
     return R / 1e5, M / Msun, (r, e_R, p_R, m_R)
 
-  def solve_tidal(self, c_dens, rmax=30e5, rtol=1.0e-4, dr=100):
-    R, M, R_dep  = self.solve(c_dens, rmax, dr=dr)
+  def solve_tidal(self, c_dens, rmax=30e5, rtol=1.0e-4, dmrel=10e-12, dr=100):
+    """
+    Solves TOV equation and calculates tidal properties 
+    for neutron star with given central density c_dens.
+
+    Parameters
+    ----------
+    c_dens : float, MeV / fm^3
+    rmax : float, cm, optional
+      Maximal distance from the star center along which star profile is calculated.
+      If rmax is smaller then actual radius of the star, then Mass and Radius 
+      will be calculated wrong.
+    rtol : float, optional
+      Relative accuracy of ODE solver.
+    dmrel : float, optional
+      Relative mass increase by which star boundary is estimated. 
+    dr : float, cm, optinal
+      Stepsize for ODE solver.
+
+    Returns
+    -------
+    [R, M, C, k2, y, beta, H], float array_like:
+      R : float, km
+        Calculated radius of the star.
+      M : float, Msun
+        Calculated mass of the star.
+      C : float, unitless
+        Compactness.
+      k2 : float, unitless
+        Second Love number.
+      y : float, unitless
+      beta : float, unitless
+      H : float, unitless
+    """
+    R, M, R_dep  = self.solve(c_dens, rmax=rmax, rtol=rtol, dmrel=dmrel, dr=dr)
     r, e_R, p_R, m_R = R_dep
 
     R *= 1e5
